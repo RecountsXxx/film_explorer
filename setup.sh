01b7f63d46799105889162cce114d239c828a409
RecountsXxx <kapriyanbogdan@ukr.net>
2024-04-25 18:07:37 +0300

    fix bug

--- setup.sh
+++ setup.sh
@@ -1,18 +1,7 @@
-cp ./back/api/src/.env.example ./back/api/src/.env
-cp ./back/api_admin/src/.env.example ./back/api_admin/src/.env
-cp ./back/api_vendor/src/.env.example ./back/api_vendor/src/.env
+cp ./project/src/.env.example ./project/src/.env
 
 docker-compose up -d --build
 
-npm i --prefix ./front/admin/src
-npm i --prefix ./front/public/src
-npm i --prefix ./front/vendor/src
-npm i --prefix ./back/sockets/src
-
-docker exec db.mysql.main mysql -h localhost -P 9090 -u root -ppassword -e "DROP DATABASE webshop;"
-docker exec db.mysql.main mysql -h localhost -P 9090 -u root -ppassword -e "CREATE DATABASE IF NOT EXISTS webshop;"
-docker exec -i db.mysql.main mysql -h localhost -P 9090 -u root -ppassword webshop < ./webshop.sql
-
-docker-compose restart
+npm i --prefix ./project/src
 
 echo "Install his completed!"
