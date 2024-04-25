8a1889f70408515f7cd38d3afcb0f3018d551aee
RecountsXxx <kapriyanbogdan@ukr.net>
2024-04-25 20:35:50 +0300

    fix bug

--- setup.sh
+++ setup.sh
@@ -2,11 +2,9 @@ cp ./project/src/.env.example ./project/src/.env
 cp .env.example .env
 
 docker-compose up -d --build
-
-docker-compose exec project.php php artisan migrate
-cd ./project/src
+cd ./project/src/
 npm install
 npm run dev
-
+docker-compose exec project.php php artisan migrate
 
 echo "Install his completed!"
